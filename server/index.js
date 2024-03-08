a6b02c7e37211c62044efd783674d9867d600253
Ryan Weisman <ry326542@ucf.edu>
2024-03-08 16:03:16 -0500

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// All other GET requests not handled before will return our React app
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
