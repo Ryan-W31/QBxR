990f5807f626e019f183967b24f5e7c84a43123f
Ryan Weisman <ry326542@ucf.edu>
2024-03-27 11:27:24 -0400

    Skeleton of Landing Page

--- server/index.js
+++ server/index.js
@@ -10,7 +10,6 @@ app.listen(PORT, () => {
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-// All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
