fe96e76feac2d41f8f4305de07269e6e0d6aae62
Ryan Weisman <ryan.weisman.512@gmail.com>
2024-05-16 11:13:16 -0400

    API url test

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../hooks/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5001/api",
+  baseUrl: "http://qbxr-env.eba-mzjrqevn.us-east-1.elasticbeanstalk.com/api",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
