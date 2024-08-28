e709ffc3baa916344016164a10725ba11efeab0d
Ryan Weisman <ry326542@ucf.edu>
2024-08-28 14:21:02 -0400

    Added Caddy support for SSL and reverse proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000 },
+  server: {
+    host: true,
+  },
   test: {
     environment: "jsdom",
     globals: true,
