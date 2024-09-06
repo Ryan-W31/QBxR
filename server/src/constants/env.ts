ce64e30aa9e364e4ffd380955f748b98d265e775
Ryan Weisman <ry326542@ucf.edu>
2024-09-06 18:05:20 -0400

    Changed env defaults

--- server/src/constants/env.ts
+++ server/src/constants/env.ts
@@ -8,9 +8,9 @@ const getEnv = (key: string, defaultValue?: string): string => {
 
 export const NODE_ENV = getEnv("NODE_ENV", "development");
 export const PORT = getEnv("PORT", "5001");
-export const MONGODB_URI = getEnv("MONGODB_URI");
-export const APP_ORIGIN_URL = getEnv("APP_ORIGIN_URL");
+export const MONGODB_URI = getEnv("MONGODB_URI", "mongodb://localhost:27017/test");
+export const APP_ORIGIN_URL = getEnv("APP_ORIGIN_URL", "http://localhost:3000");
 export const JWT_ACCESS_SECRET = getEnv("JWT_ACCESS_SECRET");
 export const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET");
-export const EMAIL_SENDER = getEnv("EMAIL_SENDER");
+export const EMAIL_SENDER = getEnv("EMAIL_SENDER", "noreply@localhost");
 export const RESEND_API_KEY = getEnv("RESEND_API_KEY");
